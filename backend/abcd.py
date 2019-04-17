144aac7bc3057256254d13a5071a1e750ba6022f
Ranjith Cheguri <theeverindian@gmail.com>
2019-04-17 15:34:42 -0700

    backend processing completed

---
+++ backend/abcd.py
@@ -0,0 +1,15 @@
+BANDWIDTHS = [
+    {'timestamp': 100, 'bytes_ts': 1 , 'bytes_fs': 10, 'device_id': 'abc'},
+    {'timestamp': 90, 'bytes_ts': 2 , 'bytes_fs': 12, 'device_id': 'abc'},
+    {'timestamp': 80, 'bytes_ts': 3 , 'bytes_fs': 13, 'device_id': 'abc'},
+    {'timestamp': 70, 'bytes_ts': 4 , 'bytes_fs': 14, 'device_id': 'abc'},
+    {'timestamp': 60, 'bytes_ts': 5 , 'bytes_fs': 16, 'device_id': 'abc'},
+    {'timestamp': 50, 'bytes_ts': 6 , 'bytes_fs': 11, 'device_id': 'abc'},
+    {'timestamp': 40, 'bytes_ts': 8 , 'bytes_fs': 102, 'device_id': 'abc'},
+    {'timestamp': 30, 'bytes_ts': 7 , 'bytes_fs': 103, 'device_id': 'abc'},
+    {'timestamp': 20, 'bytes_ts': 9 , 'bytes_fs': 104, 'device_id': 'abc'},
+    {'timestamp': 10, 'bytes_ts': 10 , 'bytes_fs': 105, 'device_id': 'abc'},
+    {'timestamp': 110, 'bytes_ts': 11 , 'bytes_fs': 150, 'device_id': 'abc'},
+    {'timestamp': 120, 'bytes_ts': 12, 'bytes_fs': 105, 'device_id': 'abc'},
+
+]
--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import time
+from datetime import datetime
+import json
+from bandwidths import BANDWIDTHS
 
 app = Flask('__main__')
 
 
 @app.route('/')
 def index():
-    return "Landing Page"
+    return "Hello Flask"
 
 
-@app.route('/hello')
-def hello():
-    return render_template('hello.html')
+@app.route('/getData', methods=['GET'])
+def getData():
+    device_uuid = request.args.get('device_uuid')
+    end_time = request.args.get('end_time', default=time.time())
+    window_time = request.args.get('window_time', default=60, type=int)
+    num_windows = request.args.get('num_windows', default=10, type=int)
+
+    device_uuid = "04827402-0725-437c-bc22-c9670ea084f0"
+    end_time = time.time()
+    output = list(filter(lambda item: item["device_id"] == device_uuid and item['timestamp'] <= end_time, BANDWIDTHS))
+    if(len(output)>0):
+        start = output[0]["timestamp"]
+        temp=[start,output[0]["bytes_ts"],output[0]["bytes_fs"]]
+        temp_window_time =0
+        temp_window_num=0
+        ans=[]
+        for i in range(1,len(output)):
+            temp_window_time = start - output[i]["timestamp"]
+            if(temp_window_time<=window_time):
+                temp[1]+=output[i]["bytes_ts"]
+                temp[2]+=output[i]["bytes_fs"]
+            else:
+                ans.append(temp[:])
+                temp_window_num+=1
+                start = output[i]["timestamp"]
+                temp=[start,output[i]["bytes_ts"],output[i]["bytes_fs"]]
+                temp_window_time =0
+            if(temp_window_num>=num_windows-1):
+                break
+        ans.append(temp)
+        return(str(ans))
+    else:
+        return('invalid device id')    
 
 
-#if __name__ == '__main__':
+# if __name__ == '__main__':
 app.run(debug=True)
